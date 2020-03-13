bb3b8d2223a016503af7059a050ee2d06e0f549e
DiogoPires <DPI@impact.dk>
2020-03-13 16:37:31 +0100

    Vai

--- Estudo/Estudo/Controllers/ValuesController.cs
+++ Estudo/Estudo/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace Estudo.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value4", "value5" };
+            return new string[] { "value7", "value5" };
         }
 
         // GET api/values/5
